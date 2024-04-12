7087136fb199f0148508cb07f1ef38b1f7a09483
EVISION\Otabek.Mirzakhmedov <otabek.mirzakhmedov@wolterskluwer.com>
2024-04-12 10:29:42 +0500

    retry

--- src/functions-tasks.js
+++ src/functions-tasks.js
@@ -146,8 +146,18 @@ function memoize(func) {
  * }, 2);
  * retryer() => 2
  */
-function retry(/* func, attempts */) {
-  throw new Error('Not implemented');
+function retry(func, attempts) {
+  return function retrier() {
+    for (let i = 0; i < attempts; i += 1) {
+      try {
+        return func();
+      } catch (error) {
+        // If an error is thrown, retry until the attempts are exhausted
+      }
+    }
+
+    throw new Error('All attempts failed');
+  };
 }
 
 /**
